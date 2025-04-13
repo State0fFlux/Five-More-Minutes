001b155faaea01626b7515b0c526dd0dc276b27b
State0fFlux <bmanske505@gmail.com>
2025-04-13 13:24:09 -0700

    Added changing skies

--- Prefabs/sheep.gd
+++ Prefabs/sheep.gd
@@ -10,7 +10,7 @@ const JUMP_VELOCITY = -50.0
 const BOOST = 2
 const SPEED = 20
 
-var state = Global.SheepState.SLEEPING
+var state
 var value = 5 # the amount of time that will pass with this sheep
 
 signal crashed
@@ -22,20 +22,22 @@ func _physics_process(delta: float) -> void:
 	# Handle out-of-bounds
 	if global_position.x > deathPoint.global_position.x:
 		queue_free()
+		
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 	
 	match state:
 		Global.SheepState.SLEEPING:
 			if Input.is_action_just_pressed("Jump"):
-				change_state(Global.SheepState.MOVING)
+				set_state(Global.SheepState.MOVING)
 		Global.SheepState.MOVING:
-			# Add the gravity.
-			if not is_on_floor():
-				anim.play("Jump")
-				velocity += get_gravity() * delta
-			else:
+			if is_on_floor():
 				velocity.x = SPEED
 				# Run across screen
 				anim.play("Walk")
+			else:
+				anim.play("Jump")
 				
 			# Handle jump.
 			if Input.is_action_just_pressed("Jump") and is_on_floor():
@@ -52,16 +54,20 @@ func emit_snores(emitting: bool):
 	particles.texture = Global.snore
 	particles.emitting = emitting
 
-func change_state(newState: Global.SheepState):
-	if state != newState:
+func set_state(newState: Global.SheepState):
+	#if state != newState:
 		match newState:
 			Global.SheepState.MOVING:
 				emit_snores(false)
+				if state == Global.SheepState.SLEEPING:
+					anim.play("Wake")
+					await anim.animation_finished
 			Global.SheepState.SLEEPING:
 				velocity.x = 0
 				emit_snores(true)
+				anim.play("Sleep")
 			Global.SheepState.DEAD:
-				velocity.x = 0
+				velocity = Vector2.DOWN
 				emit_snores(false)
 				anim.play("Die")
 				var tween = create_tween().set_parallel(false)
--- Scenes/Dream.gd
+++ Scenes/Dream.gd
@@ -1,34 +1,46 @@
 extends Node2D
 
 const SheepScene = preload("res://Prefabs/sheep.tscn")
-@onready var spawnPoint = $Camera2D/SpawnPoint
+@export var TIME_MULT: int = 1
+
+@onready var spawnPoint = $Camera2D/SpawnPoint.global_position
 
 var active = true
 
+func _ready() -> void:
+	var sheep = spawn_sheep($FirstSheepSpawn.global_position, Global.SheepState.SLEEPING)
+	
 func _process(delta: float) -> void:
 	if active:
 		# passive time progression
-		Global.minutes_since_midnight += delta
+		Global.minutes_since_midnight += delta * TIME_MULT
 	
-func spawn_sheep():
+func spawn_sheep(spawnPoint: Vector2, state: Global.SheepState):
 	var sheep = SheepScene.instantiate()
 	sheep.value = Global.VARIANTS.keys()[randi_range(0, Global.VARIANTS.size() - 1)]
-	sheep.global_position = spawnPoint.global_position
+	sheep.global_position = spawnPoint
 	sheep.connect("crashed", _on_sheep_crashed)
 	sheep.add_to_group("sheep")
 	add_child(sheep)
-	sheep.change_state(Global.SheepState.MOVING)
-
-func _on_wall_body_entered(body: Node2D) -> void:
-	if body.is_in_group("sheep"):
-		if body.is_on_floor(): # sheep crashed into fence
-			body.change_state(Global.SheepState.DEAD)
-		else: # sheep jumped the fence
-			Global.minutes_since_midnight += body.value
-			body.emit_hearts(true)
-			spawn_sheep()
+	sheep.set_state(state)
+	return sheep
 
 
 func _on_sheep_crashed() -> void:
 	active = false
 	Global.battery -= 25
+
+
+func _on_death_wall_body_entered(body: Node2D) -> void:
+	if body.is_in_group("sheep"): # sheep crashed into fence
+		body.set_state(Global.SheepState.DEAD)
+
+
+func _on_pass_wall_body_entered(body: Node2D) -> void:
+	if body.is_in_group("sheep") and body.state == Global.SheepState.MOVING: # sheep jumped the fence
+		var tween = create_tween()
+		var new_time = Global.minutes_since_midnight + body.value
+		tween.tween_property(Global, "minutes_since_midnight",
+			new_time, 2.0).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+		body.emit_hearts(true)
+		spawn_sheep(spawnPoint, Global.SheepState.MOVING)
