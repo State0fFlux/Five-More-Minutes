ecf302614a3e9832f17f017667a1612c38bf8570
Avah <114538590+zambiers@users.noreply.github.com>
2025-04-15 22:18:18 -0700

    Got the start menu design up

---
+++ Scenes/start_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+func _on_wake_up_button_pressed() -> void:
+	print("wake up pressed")
+
+
+func _on_options_button_pressed() -> void:
+	print("options pressed")
+
+
+func _on_meet_the_devs_button_pressed() -> void:
+	print("devs pressed")
+
+func _on_quit_button_pressed() -> void:
+	print("quit pressed")
