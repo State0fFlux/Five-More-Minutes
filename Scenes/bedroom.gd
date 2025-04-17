bd2d2468fb07c56a218815bb98502d3c58ca18a2
State0fFlux <bmanske505@gmail.com>
2025-04-17 02:23:50 -0700

    Adding dream zoom in/out
    next up: incorporate a way for dream boundary to expand after zooming in (immerse self into the dream, idrk how to do this though)

--- Scenes/bedroom.gd
+++ Scenes/bedroom.gd
@@ -9,16 +9,33 @@ func _ready() -> void:
 	anim.play("PhoneUp")
 	
 func on_dream_state_changed():
-	if Global.is_dreaming:
+	if Global.is_dreaming:		
 		anim.play("PhoneDown")
+		await anim.animation_finished
+		
+		# zoom in camera
+		var tween = create_tween().set_parallel(true)
+		tween.tween_property($Camera2D, "offset", Vector2(0, -30), 4)
+		tween.tween_property($Camera2D, "zoom", Vector2(4, 4), 4)
+		
+		#anim.play("CamZoomIn"), deleted bc bad interleaving with parallel anims
 	else:
-		anim.play("PhoneUp")
+		# zoom out camera
+		var tween = create_tween().set_parallel(true)
+		tween.tween_property($Camera2D, "offset", Vector2(0, 0), 0.5)
+		tween.tween_property($Camera2D, "zoom", Vector2(1, 1), 0.5)
+		
+		anim.play("PhoneUp") # TODO: require clicking of phone instead of auto-pickup
 
 func _on_snooze_pressed() -> void:
 	Global.set_dream_state(true)
 	player.set_state(Global.PlayerState.SLEEPING)
 	
 func _on_get_up_pressed() -> void:
-	anim.play("TextIntro")
-	await get_tree().create_timer(2.0).timeout
-	anim.play("TextExit")
+	var tween = create_tween()
+	tween.tween_property($Title, "visible_ratio", 1.0, 1)
+	await get_tree().create_timer(3.0).timeout
+	$Title.visible_ratio = 0
+	#anim.play("TextIntro")
+	#await get_tree().create_timer(2.0).timeout
+	#anim.play("TextExit")
