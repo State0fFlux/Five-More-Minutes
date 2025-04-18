4937f313173fa89acc507bb8c9e916ca24418f68
Avah <114538590+zambiers@users.noreply.github.com>
2025-04-18 15:48:14 -0700

    Revert "Got the sliders almost done"
    This reverts commit e2676a10c7eeccef59317acf681fc1a4564ed93c.

--- Scenes/options.gd
+++ Scenes/options.gd
@@ -2,8 +2,3 @@ extends Control
 
 func _on_home_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Start_menu.tscn")
-
-
-func _on_save_button_pressed() -> void:
-	AudioServer.set_bus_volume_db(1, linear_to_db($"Audio/Volumes/Music slider".value))
-	AudioServer.set_bus_volume_db(2, linear_to_db($"Audio/Volumes/SFX slider".value))
