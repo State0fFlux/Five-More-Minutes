bef6c696e895574270bb72550d2bc0aed15da612
Avah <114538590+zambiers@users.noreply.github.com>
2025-04-18 17:28:17 -0700

    Finished the credits and win/lose screens.

--- Scenes/credits.gd
+++ Scenes/credits.gd
@@ -1,7 +1,9 @@
 extends Control
 
 func _on_options_pressed() -> void:
+	$AudioStreamPlayer.play()
 	get_tree().change_scene_to_file("res://Scenes/Options.tscn")
 
 func _on_home_pressed() -> void:
+	$AudioStreamPlayer.play()
 	get_tree().change_scene_to_file("res://Scenes/Start_menu.tscn")
--- Scenes/start_menu.gd
+++ Scenes/start_menu.gd
@@ -1,13 +1,17 @@
 extends Control
 
 func _on_quit_button_pressed() -> void:
+	$AudioStreamPlayer.play()
 	get_tree().quit()
 
 func _on_wake_up_button_pressed() -> void:
+	$AudioStreamPlayer.play()
 	get_tree().change_scene_to_file("res://Scenes/Bedroom.tscn")
 
 func _on_options_button_pressed() -> void:
+	$AudioStreamPlayer.play()
 	get_tree().change_scene_to_file("res://Scenes/Options.tscn")
 
 func _on_meet_the_devs_button_pressed() -> void:
+	$AudioStreamPlayer.play()
 	get_tree().change_scene_to_file("res://Scenes/Credits.tscn")
