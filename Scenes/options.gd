26b4528d6dd78631219ef1658f502433ae31dd12
State0fFlux <bmanske505@gmail.com>
2025-04-25 19:11:34 -0700

    removing all uppercase stuff

--- Scenes/options.gd
+++
@@ -1,5 +0,0 @@
-extends Control
-
-func _on_home_button_pressed() -> void:
-	$AudioStreamPlayer.play()
-	get_tree().change_scene_to_file("res://scenes/start.tscn")
--- scenes/creds.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-func _on_options_pressed() -> void:
-	$AudioStreamPlayer.play()
-	get_tree().change_scene_to_file("res://scenes/options.tscn")
-
-func _on_home_pressed() -> void:
-	$AudioStreamPlayer.play()
-	get_tree().change_scene_to_file("res://scenes/start.tscn")
